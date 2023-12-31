18ed6b043db5ad5ca8a5cc4bb393d679de248660
Gaurav Kumar <gauravkr2179@gmail.com>
2023-12-31 17:36:37 +0530

    added logic to fetch coin

--- src/components/ConverterForm.js
+++ src/components/ConverterForm.js
@@ -1,34 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/styles.css";
+import axios from "axios";
 
 const ConverterForm = () => {
+  const [cryptoCurrenies, setCryptoCurrenies] = useState([]);
+  const [cryptocoin, setCryptoCoin] = useState("");
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("USD");
+  const [convertedAmount, setConvertedAmount] = useState("");
+  useEffect(() => {
+    const fetchCryptoCurrencies = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3003/dzap/cryptoCurrencies"
+        );
+        console.log(response.cryptocurrencies);
+        setCryptoCurrenies(response);
+      } catch (error) {
+        console.error("Error in fetching crypto currency", error);
+      }
+    };
+    fetchCryptoCurrencies();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("", {
+        cryptocoin,
+        amount,
+        currency,
+      });
+      setConvertedAmount(res.data);
+    } catch (error) {
+      console.error("Error in converting currency", error);
+    }
+  };
   return (
-    <div>
-      <h1>Crypto Currency Converter</h1>
-      <div>
-        <form>
-          <div>
+    <div className="convert-container">
+      <div className="converter-form">
+        <h1>Crypto Currency Converter</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
             <label>
               Crypto Currency
-              <select>
-                <option value="USD">USD</option>
+              <select
+                value={cryptocoin}
+                onChange={(e) => setCryptoCoin(e.target.value)}
+              >
+                <option value="">Select Crypto Currency</option>
+                {cryptoCurrenies.map((coin) => (
+                  <option key={coin.id} value={coin}>
+                    {coin}
+                  </option>
+                ))}
               </select>
             </label>
           </div>
-          <div>
+          <div className="form-group">
             <label>
               Crypto Amount
-              <input />
+              <input
+                type="number"
+                value={amount}
+                placeholder="Enter Crypto Amount"
+                onChange={(e) => setAmount(e.target.value)}
+              />
             </label>
           </div>
-          <div>
+          <div className="form-group">
             <label>
               Select Currency
-              <select>
-                <option value="USD">USD</option>
+              <select
+                value={currency}
+                onChange={(e) => setCurrency(e.target.value)}
+              >
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+                <option value="inr">INR</option>
+                <option value="jpy">JPY</option>
               </select>
             </label>
           </div>
+          <div className="form-group">
+            <button type="Submit"> Convert</button>
+          </div>
         </form>
+        {convertedAmount && (
+          <p>
+            Converted Amount: {convertedAmount} {currency}
+          </p>
+        )}
       </div>
     </div>
   );
